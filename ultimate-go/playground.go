68f68e39b021441d722939064566ee3e8036feb1
roy-santos <roysantos112@gmail.com>
2022-12-10 14:28:00 -0800

    Add playground files.

--- ultimate-go/playground.go
+++ ultimate-go/playground.go
@@ -119,4 +119,101 @@ func main() {
 
 	// implicit conversion between named and unamed type is allowed
 	b1 = e4 // this is OK!
+
+	// Declare variable of type int with a value of 10.
+	count := 10
+
+	/*
+		Value Semantics - Pass by value, passes copy of the value across program boundaries
+	*/
+
+	// Display the "value of" and "address of" count.
+	println("count:\tValue Of[", count, "]\tAddr Of[", &count, "]")
+
+	// Pass the "value of" the count.
+	incrementByValue(count)
+
+	println("count:\tValue Of[", count, "]\tAddr Of[", &count, "]")
+
+	/*
+		Pointer Semantics - Pass address of the value, passes memory location of the value across program boundaries
+	*/
+
+	// Declare variable of type int with a value of 10.
+	count2 := 10
+
+	// Display the "value of" and "address of" count.
+	println("count:\tValue Of[", count2, "]\tAddr Of[", &count2, "]")
+
+	// Pass the "address of" count.
+	incrementByReference(&count2)
+
+	println("count:\tValue Of[", count2, "]\tAddr Of[", &count2, "]")
+
+	/*
+		Escape Analysis -  a type of static code analysis done by compiler, reads code at compile time and determines whether
+		a value should be constructed on the stack or on the heap (an escape) based on how the value is shared
+	*/
+
+	u1 := createUserV1() // copy of value is received from the fxn call
+	u2 := createUserV2() // fxn call returns address of the value located on the heap
+
+	// note: values created on the heap have to get managed by garbage collector, anything on the stack is self cleaning
+	// there is a tradeoff here. Efficiency vs data integrity
+
+	print("u1", &u1, "u2", u2)
+
+	/*
+		Stack Growth -
+	*/
+}
+
+// increment declares count as a pointer variable whose value is always an address and points to values of type int.
+func incrementByValue(inc int) {
+	inc++
+	println("inc:\tValue Of[", inc, "]\tAddr Of[", &inc, "]")
+
+}
+
+// increment declares count as a pointer variable whose value is always an address and points to values of type int.
+// this is still "pass by value" where the value being passed is the address.
+func incrementByReference(inc *int) { // *int type is address of the int, not the int itself
+	// Increment the "value of" count that the "pointer points to".
+	// this is indirect access to the original value
+	// direct access to a value can only be done if the value is declared within the "frame (in this case, the fxn boundary)"
+	*inc++
+	println("inc:\tValue Of[", inc, "]\tAddr Of[", &inc, "]")
+
+}
+
+// user represents a user in the system.
+type user struct {
+	name  string
+	email string
+}
+
+// createUserV1 creates a user value and passes a copy back to the caller
+// value semantic function, sends COPY of the user value back
+func createUserV1() user {
+	userV1 := user{
+		name:  "John",
+		email: "john@gmail.com",
+	}
+
+	println("V1", &userV1)
+
+	return userV1
+}
+
+// createUserV2 creates a user value and shares the value with the caller
+// pointer semantic function, shares original copy of the user value w/ caller
+func createUserV2() *user {
+	userV2 := user{ // this value gets created on the heap, represents a value on the heap
+		name:  "John",
+		email: "john@gmail.com",
+	}
+
+	println("V2", &userV2)
+
+	return &userV2 // shares value up call stack, compiler knows to construct value in the heap (escape analysis)
 }
