f6c07d37750bb71e1dc696b4ce6be40436a8fe31
roy-santos <roysantos112@gmail.com>
2022-12-14 16:23:00 -0800

    Finished Day1 part 2.

--- practical-go-foundations/code/banner/banner.go
+++ practical-go-foundations/code/banner/banner.go
@@ -1,14 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	banner("Go", 6)
 	banner("G☺", 6)
+
+	s := "G☺"
+	fmt.Println("len:", len(s))
+	// rune = code points ~= unicode character
+
+	for i, r := range s {
+		fmt.Println(i, r)
+		if i == 0 {
+			fmt.Printf("%c of type %T\n", r, r)
+			// rune (int32)
+		}
+	}
+
+	b := s[0]
+	fmt.Printf("%c of type %T\n", b, b)
+	// byte (uint8)
+
+	x, y := 1, "1"
+	fmt.Printf("x=%v, y=%v\n", x, y)
+	fmt.Printf("x=%#v, y=%#v\n", x, y) // use #v in debug/log, shows type
+
+	fmt.Printf("%20s!\n", s) // prints 20 spaces then s variable
+
+	fmt.Println(isPalindrome("g"))
+	fmt.Println(isPalindrome("go"))
+	fmt.Println(isPalindrome("gog"))
+	fmt.Println(isPalindrome("g☺g"))
+}
+
+// isPalindrome("g") -> true
+// isPalindrome("go") -> false
+// isPalindrome("gog") -> true
+// isPalindrome("gogo") -> true
+func isPalindrome(s string) bool {
+	rs := []rune(s) // get slice of rune, makes code unicode aware
+	for i := 0; i < (len(s))/2; i++ {
+		if rs[i] != rs[len(rs)-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 func banner(text string, width int) {
-	padding := (width - len(text)) / 2
+	padding := (width - utf8.RuneCountInString(text)) / 2
+	// padding := (width - len(text)) / 2 / 2 // BUG: len is in bytes
 	for i := 0; i < padding; i++ {
 		fmt.Print(" ")
 	}
